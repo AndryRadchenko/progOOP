e16a4c11ebc62b554ab7cf2929d79b1153d6d5bc
Andrey Radchenko <andry.radchenko@gmail.com>
2017-10-29 22:26:45 +0200

    A TreeMap object/frequency is used instead of 2 HashMaps (hashcobe/object and hashcode/frequency)

--- Lesson10_MapHoWo/src/ex03_elements/Main.java
+++ Lesson10_MapHoWo/src/ex03_elements/Main.java
@@ -1,66 +1,58 @@
 package ex03_elements;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
+import java.util.TreeMap;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 
-//		массив прямоугольников
+		// массив прямоугольников
 		Rectangle[] arrayOne = new Rectangle[100000];
 		Random rn = new Random();
-		for (int i = 0; i < arrayOne.length; i++) 
+		for (int i = 0; i < arrayOne.length; i++)
 			arrayOne[i] = new Rectangle((rn.nextInt(3) + 3), (rn.nextInt(4) + 5));
-		
-//		массив строк
+
+		// массив строк
 		String[] arrayTwo = new String[100000];
 		Random rns = new Random();
 		for (int i = 0; i < arrayOne.length; i++)
 			arrayTwo[i] = "String " + rns.nextInt(10);
+
+		tmapFill(arrayOne);
+		tmapFill(arrayTwo);
 		
-		aggregate(arrayOne);
-		aggregate(arrayTwo);
 	}
 
-	// карта для пар хэшкод: частота
-	public static HashMap<Integer, Integer> hmapFill(Object[] array) {
-		HashMap<Integer, Integer> hmap = new HashMap<>();
+	// карта для пар объект: частота
+	public static void tmapFill(Object[] array) {
+		TreeMap<Object, Integer> tmap = new TreeMap<>(new Comparator<Object>(){
+			@Override
+			 public int compare(Object letFr0, Object letFr1) {
+			 if (letFr0.hashCode() > letFr1.hashCode()) {
+			 return -1;
+			 } else if (letFr0.hashCode() < letFr1.hashCode()) {
+			 return 1;
+			 } else
+			 return 0;
+			 }
+		});
+
 		for (Object obj : array) {
-			Integer hshcode = obj.hashCode();
-			Integer hmapElem = hmap.get(hshcode);
-			if (hmapElem != null)
-				hmap.put(hshcode, hmapElem + 1);
+			Integer tmapFreq = tmap.get(obj);
+			if (tmapFreq != null)
+				tmap.put(obj, tmapFreq + 1);
 			else
-				hmap.put(hshcode, 1);
+				tmap.put(obj, 1);
 		}
-		return hmap;
-	}
 
-	// карта для пар хэшкод: объект
-	public static HashMap<Integer, Object> omapFill(Object[] array) {
-		HashMap<Integer, Object> omap = new HashMap<>();
-		for (Object obj : array) {
-			Integer hshcode = obj.hashCode();
-			Object omapElem = omap.get(hshcode);
-			if (omapElem == null)
-				omap.put(hshcode, obj);
-		}
-		return omap;
-	}
+		Set<Object> set = tmap.keySet();
+		for (Object obj : set)
+			System.out.println(obj + " -> " + tmap.get(obj));
 
-//	вывод "объект -> количество"
-	public static void comparisonPrint(HashMap<Integer, Integer> hmap, HashMap<Integer, Object> omap) {
-		Set<Integer> set = hmap.keySet();
-		for (Integer hshcode : set)
-			System.out.println(omap.get(hshcode) + " -> " + hmap.get(hshcode));
-	}
-	
-//	функция-агрегатор
-	public static void aggregate(Object[] array){
-		HashMap<Integer, Integer> hmap = hmapFill(array);
-		HashMap<Integer, Object> omap = omapFill(array);
-		comparisonPrint(hmap, omap);
 	}
 }
